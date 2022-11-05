5cc0c78c1adab20754b7bded6ac762cd5443bc9e
Baha Serdaroglu <73171601+developerbaha@users.noreply.github.com>
2022-11-05 16:11:05 +0000

    Update main.py
    can clean stop words and get multiple websties

--- main.py
+++ main.py
@@ -7,19 +7,33 @@ import urllib
 import urlopen
 from nltk.probability import FreqDist
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import string
+from nltk.util import bigrams, trigrams, ngrams
 
 # providing url
 fdist = FreqDist()
-response = requests.get('https://parts.igem.org/Part:BBa_R0040').content
-soup = BeautifulSoup(response, "html.parser")
-text_tokens = nltk.tokenize.word_tokenize(soup.get_text())
-punctuation = re.compile(r"[.?!,:;()|0-9]")
-post_punctuation = []
-for word in text_tokens:
-    word = punctuation.sub("", word)
-    if len(word) > 0:
-        post_punctuation.append(word)
-for word in post_punctuation:
-    fdist[word.lower()] += 1
-    fdist1 = fdist.most_common(50)
+all_stopwords = stopwords.words('english')
+list_stopwords = ['the', 'part', 'page', 'iptg', 'MM', 'IPTG', 'Part', 'The', 'mM', '-', 'R', 'There']
+all_stopwords.extend(list_stopwords)
+
+base_url = ('https://parts.igem.org/Part:BBa_R00')
+page_number = 10
+while page_number != 200:
+    url = base_url + str(page_number)
+    response = requests.get(url).text
+    soup = BeautifulSoup(response, "html.parser")
+    page_number = page_number + 1
+    punctuation = re.compile(r'[.?!,:;()|%]')
+    post_punctuation = []
+    text_tokens = nltk.tokenize.word_tokenize(soup.get_text())
+    tokens_without_sw = [word for word in text_tokens if not word in all_stopwords]
+    for word in tokens_without_sw:
+        word = punctuation.sub("", word)
+        if len(word) > 0:
+            post_punctuation.append(word)
+    for word in post_punctuation:
+        fdist[word.lower()] += 1
+        fdist1 = fdist.most_common(50)
 print(fdist1)
